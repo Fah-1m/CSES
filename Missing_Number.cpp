dbbaee809c2c16b7a1134e9eab027506f6b7fb4f
Fah-1m <fahimrahman1643@gmail.com>
2025-05-14 10:24:14 +0600

    002

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 #define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define endl '\n'
+int a[200001];
 
 int main() {
     fast;
     int t; cin >> t;
-    while(t--){
-        
+    int p = t - 1;
+    while(p--){
+        int n; cin >> n;
+        a[n]++;
+    }
+    for(int i = 1; i <= t; i++){
+        if(a[i] == 0){
+            cout << i; break;
+        }
     }
     return 0;
 }
